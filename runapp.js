f9515e11cbca87c66b1e997d27beeaf6b67121f7
HandsomeChoco <45553863+HandsomeChoco@users.noreply.github.com>
2020-12-04 11:26:15 +0900

    Create runapp.js

---
+++ runapp.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const server = http.createServer().listen(6060);
+
+server.on('request', (req, res) => {
+  console.log('request arrived');
+  res.write(`HostName: ${process.env.HOSTNAME}`);
+  res.end();
+});
+
+server.on('connection', (socket) => {
+  console.log('connected');
+});
